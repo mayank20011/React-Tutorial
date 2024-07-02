cad025112b1f928d40d149e24ee9ce26ba41b9aa
mayank20011 <mayank.behl.mb1@gmail.com>
2024-07-02 16:54:55 +0530

    .

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -1,8 +1,9 @@
 import { Export1, Export2, Export3, Export4 } from "./export";
+import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className="h1hello">Hello</h1>
       <Export1 />
       <Export2 />
       <Export3 />
