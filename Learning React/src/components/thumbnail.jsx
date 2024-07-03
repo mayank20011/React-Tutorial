3294071efccce340a04b5ef9ffacacf4fd34be7e
mayank20011 <mayank.behl.mb1@gmail.com>
2024-07-03 16:06:36 +0530

    .

--- Learning React/src/components/thumbnail.jsx
+++ Learning React/src/components/thumbnail.jsx
@@ -1,29 +1,27 @@
 import "./thumbnail.css";
-const viewstimediv="vtd";
-const viewstimep="vtp";
+const viewstimediv = "vtd";
+const viewstimep = "vtp";
 
-function Thumbnail({imgsrc,title,channel="Traversy Media",views,time, verified=false})
-{
-    let channelhtml;
-    if(verified)
-    {
-       channelhtml=<p className="channel">{channel} ✅</p>;
-    }
-    else
-    {
-        channelhtml=channelhtml=<p className="channel">{channel}</p>;;
-    }
-
-
-    return (
+function Thumbnail({
+  imgsrc,
+  title,
+  channel = "Traversy Media",
+  views,
+  time,
+  verified = false,
+}) {
+  return (
     <div className="video">
-    <img src={imgsrc} alt="" />
-     <h4 className="title">{title}</h4>
-     {channelhtml}
-     <div className={viewstimediv}>
-      <p className={viewstimep}>{views} views . </p>
-      <p className={viewstimep}>{time}</p>
-     </div>
-    </div>);
+      <img src={imgsrc} alt="" />
+      <h4 className="title">{title}</h4>
+      <p className="channel">
+        {channel} {verified && "✅"}
+      </p>
+      <div className={viewstimediv}>
+        <p className={viewstimep}>{views} views . </p>
+        <p className={viewstimep}>{time}</p>
+      </div>
+    </div>
+  );
 }
 export default Thumbnail;
