7380ccd3801c71bfbc1fd2edcd1ae7aaed8fab5e
mayank20011 <mayank.behl.mb1@gmail.com>
2024-07-03 14:28:05 +0530

    .

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -7,19 +7,20 @@ function App() {
   let obj={
     imgsrc:"https://picsum.photos/300/150",
     title:"React Js Tutorial",
-    channel:{ytchannel},
+    channel:ytchannel,
     views:"10K",
-    time:"1 month ago"
+    time:"1 month ago",
+    verified:false
   };
 
   return (
     <>
       <h1 className="h1hello">Hello</h1>
-      <Export1/>
+      <Export1 />
       <Export2 />
       <Export3 />
       <Export4 />
-      
+
       <div className="container">
         <Thumbnail {...obj}/>
         <Thumbnail
@@ -28,12 +29,14 @@ function App() {
           channel={ytchannel}
           views="100K"
           time="10 month ago"
+          verified={true}
         />
         <Thumbnail
           imgsrc="https://picsum.photos/301/150"
           title="Node Js Tutorial"
           views="1M"
           time="1 year ago"
+          verified={true}
         />
       </div>
     </>
--- Learning React/src/components/thumbnail.jsx
+++ Learning React/src/components/thumbnail.jsx
@@ -1,13 +1,25 @@
 import "./thumbnail.css";
 const viewstimediv="vtd";
 const viewstimep="vtp";
-function Thumbnail({imgsrc,title,channel="Traversy Media",views,time})
+
+function Thumbnail({imgsrc,title,channel="Traversy Media",views,time, verified=false})
 {
+    let channelhtml;
+    if(verified)
+    {
+       channelhtml=<p className="channel">{channel} ✅</p>;
+    }
+    else
+    {
+        channelhtml=channelhtml=<p className="channel">{channel}</p>;;
+    }
+
+
     return (
     <div className="video">
     <img src={imgsrc} alt="" />
      <h4 className="title">{title}</h4>
-     <p className="channel">{channel}</p>
+     {channelhtml}
      <div className={viewstimediv}>
       <p className={viewstimep}>{views} views . </p>
       <p className={viewstimep}>{time}</p>
