78e3f264fa67397528a85e721d8d06180b9076c9
mayank20011 <mayank.behl.mb1@gmail.com>
2024-07-04 13:39:12 +0530

    .

---
+++ Learning React/src/components/button.jsx
@@ -0,0 +1,7 @@
+function Button({ children }) {
+  function handleClick(e) {
+    alert(`You Clicked on ${e.target.firstChild.data} Button`);
+  }
+  return <button onClick={handleClick}>{children}</button>;
+}
+export default Button;
---
+++ Learning React/src/components/heading.jsx
@@ -0,0 +1,5 @@
+function Heading({value="This is a Heading"})
+{
+   return <h1 style={{color:"white"}}>{value}</h1>;
+}
+export default Heading;
