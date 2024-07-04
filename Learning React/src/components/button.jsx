f2705f61b2d4e97979594c9f792c60bd91eee90e
mayank20011 <mayank.behl.mb1@gmail.com>
2024-07-04 13:53:06 +0530

    .

--- Learning React/src/components/button.jsx
+++ Learning React/src/components/button.jsx
@@ -1,7 +1,9 @@
 function Button({ children }) {
+
   function handleClick(e) {
     alert(`You Clicked on ${e.target.firstChild.data} Button`);
   }
+  
   return <button onClick={handleClick}>{children}</button>;
 }
 export default Button;
