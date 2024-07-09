2a076ee795686d711986a1b798a5b5c497244c89
mayank20011 <mayank.behl.mb1@gmail.com>
2024-07-09 16:08:46 +0530

    .

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -4,18 +4,20 @@ import Counter from "./components/Counter";
 import Button from "./components/button";
 import Timer from "./components/timer";
 
-function App()
-{
-  let [count,setCount]=useState(0);
+function App() {
+  let [count, setCount] = useState(0);
   return (
     <>
-    <h1>useState Hook, Counter</h1>
-    <Counter count={count}/>
-    <Button count={count} changeCount={setCount}>Increment ++</Button>
-    <Button count={count} changeCount={setCount}>Decreament --</Button>
-    <Timer/>
+      <h1>useState Hook, Counter</h1>
+      <Counter count={count} />
+      <Button count={count} changeCount={setCount}>
+        Increment ++
+      </Button>
+      <Button count={count} changeCount={setCount}>
+        Decreament --
+      </Button>
     </>
-  ); 
+  );
 }
 
 export default App;
