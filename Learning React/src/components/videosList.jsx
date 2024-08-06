725c7fd232f6444c44b4977fe1ce8b4bb3f27310
mayank20011 <mayank.behl.mb1@gmail.com>
2024-08-06 13:08:02 +0530

    .

--- Learning React/src/components/videosList.jsx
+++ Learning React/src/components/videosList.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import VideosContext from "../../context/VideosContext";
 import Thumbnail from "./thumbnail";
-function VideosList({editVideo, dispatch})
+function VideosList({editVideo})
 {
   const videos=useContext(VideosContext);
   return (
@@ -15,7 +15,6 @@ function VideosList({editVideo, dispatch})
           verified={video.verified}
           views={video.views}
           time={video.time}
-          dispatch={dispatch}
           id={video.id}
           editVideo={editVideo}
         />))
