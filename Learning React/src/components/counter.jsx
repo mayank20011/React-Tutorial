b7adc0b4fe3f422abfe209ec920ce45779ef676a
mayank20011 <mayank.behl.mb1@gmail.com>
2024-08-27 14:45:04 +0530

    .

--- Learning React/src/components/counter.jsx
+++ Learning React/src/components/counter.jsx
@@ -6,19 +6,29 @@ function Counter() {
 
   let render = useRef(1);
 
+  // changing DOM.
+  const divRef=useRef("");
+  function changeshadow()
+  {
+      let r=Math.floor(Math.random()*256);
+      let g=Math.floor(Math.random()*256);
+      let b=Math.floor(Math.random()*256);
+      let color=`rgb(${r}, ${g}, ${b})`;
+      divRef.current.style.boxShadow=`10px 10px 5px ${color}`;
+  }
+   
   function addCount() {
     count++;
     setCount(count);
     render.current++;
     console.log(`No of renders for counter component are: ${render.current}`);
+    changeshadow();
   }
-
   return (
-    <div className="container countContainer">
+    <div className="container countContainer" ref={divRef}>
       <p>current Count is : {count}</p>
       <button onClick={addCount}>Add Count</button>
     </div>
   );
 }
-
 export default Counter;
