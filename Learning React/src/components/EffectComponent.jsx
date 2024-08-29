196f96319cf9d2a53b74cdcfa07a3dc3344e2225
mayank20011 <mayank.behl.mb1@gmail.com>
2024-08-29 14:37:03 +0530

    .

--- Learning React/src/components/EffectComponent.jsx
+++ Learning React/src/components/EffectComponent.jsx
@@ -1,24 +1,40 @@
 import { useRef, useEffect, useState } from "react";
 function EffectComponent() {
   const inputbox = useRef("");
-  let i=useRef(0);
+  let i = useRef(0);
   const [deletee, setDeletee] = useState(false);
+
   useEffect(() => {
+    let intervalid;
     if (!deletee) {
       inputbox.current.style.boxShadow = "5px 5px 5px red";
-      setInterval(()=>{
+      intervalid = setInterval(() => {
         console.log(`value of i is: ${i.current}`);
         i.current++;
-      },1000);
+      }, 1000);
     }
-  }, []);
+
+    return () => {
+      console.log('Cleanup function runs now...');
+      clearInterval(intervalid);
+    };
+
+  }, [deletee]); 
+
   function remove() {
     setDeletee(true);
   }
   return (
     !deletee && (
       <div
-        style={{ display: "block", marginBottom: "20px", width: "fit-content", border:"2px solid #ccc", padding:"5px"}}>
+        style={{
+          display: "block",
+          marginBottom: "20px",
+          width: "fit-content",
+          border: "2px solid #ccc",
+          padding: "5px",
+        }}
+      >
         <h3
           style={{
             marin: "0",
@@ -28,7 +44,8 @@ function EffectComponent() {
             textAlign: "right",
             cursor: "pointer",
           }}
-          onClick={remove}>
+          onClick={remove}
+        >
           X
         </h3>
         <input type="text" ref={inputbox} />
