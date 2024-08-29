ad3e605004ce1b01e44fc2ac8c135903b66e34aa
mayank20011 <mayank.behl.mb1@gmail.com>
2024-08-29 13:47:42 +0530

    .

--- Learning React/src/components/EffectComponent.jsx
+++ Learning React/src/components/EffectComponent.jsx
@@ -1,13 +1,34 @@
-import { useRef, useEffect } from "react";
-
+import { useRef, useEffect, useState } from "react";
 function EffectComponent() {
   const inputbox = useRef("");
-
+  const [deletee, setDeletee] = useState(false);
   useEffect(() => {
-    inputbox.current.style.boxShadow = "5px 5px 5px red";
+    if (!deletee) {
+      inputbox.current.style.boxShadow = "5px 5px 5px red";
+    }
   }, []);
-
-  return <input style={{display:"block", marginBottom:"20px"}} type="text" ref={inputbox} />;
+  function remove() {
+    setDeletee(true);
+  }
+  return (
+    !deletee && (
+      <div
+        style={{ display: "block", marginBottom: "20px", width: "fit-content", border:"2px solid #ccc", padding:"5px"}}>
+        <h3
+          style={{
+            marin: "0",
+            padding: "0",
+            right: "0px",
+            position: "absoluute",
+            textAlign: "right",
+            cursor: "pointer",
+          }}
+          onClick={remove}>
+          X
+        </h3>
+        <input type="text" ref={inputbox} />
+      </div>
+    )
+  );
 }
-
 export default EffectComponent;
